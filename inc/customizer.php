1712bd2b5981b9000eb070d54ea75c55d3f3684a
Robert DeVore <deviodigital@gmail.com>
2018-09-19 06:35:12 -0400

    :bug: BUGFIX: re-added extra 0's for h1-h3 weight number in the customizer

--- inc/customizer.php
+++ inc/customizer.php
@@ -1660,7 +1660,7 @@ function cannabiz_customizer_css() {
 	<?php } ?>
 	<?php if ( get_theme_mod( 'cannabiz_title_font_weight' ) !='' ) { ?>
 		h1, h2, h3 {
-			font-weight: <?php echo get_theme_mod( 'cannabiz_title_font_weight' ); ?>;
+			font-weight: <?php echo get_theme_mod( 'cannabiz_title_font_weight' ); ?>00;
 		}
 	<?php } ?>
 
