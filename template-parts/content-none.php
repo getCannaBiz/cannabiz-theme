e2f46b820fdfa17a85eca7adee37876eb82382de
Robert DeVore <deviodigital@gmail.com>
2018-10-07 10:41:25 -0400

    :bug: BUG: Fixed content display in 404 error pages

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -9,12 +9,9 @@
 
 ?>
 
-<section class="no-results not-found">
-	<header class="page-header">
+<article class="hentry no-results not-found">
+	<div class="entry-content">
 		<h1 class="entry-title"><?php esc_html_e( 'Nothing Found', 'cannabiz' ); ?></h1>
-	</header><!-- .page-header -->
-
-	<div class="page-content">
 		<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
 
 			<p><?php printf( wp_kses( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'cannabiz' ), array( 'a' => array( 'href' => array() ) ) ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
@@ -31,4 +28,4 @@
 
 		<?php endif; ?>
 	</div><!-- .page-content -->
-</section><!-- .no-results -->
+</article><!-- .no-results -->
