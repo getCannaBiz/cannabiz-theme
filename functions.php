c4fd024bbbeb248f3b2685d4907d5ae329e599a5
Robert DeVore <deviodigital@gmail.com>
2020-11-16 23:09:22 -0500

    :ok_hand: IMPROVE: Removed 'CannaBiz' link from WPD admin_menu links

--- functions.php
+++ functions.php
@@ -321,17 +321,8 @@ function cannabiz_paypal_button( $translated_text, $text, $domain ) {
 add_filter( 'gettext', 'cannabiz_paypal_button', 20, 3 );
 
 /**
- * Add "CannaBiz" submenu to WPD admin menu
- */
-add_action( 'admin_menu', 'wpd_cannabiz_submenu_page', 9 );
-function wpd_cannabiz_submenu_page() {
-	add_submenu_page( 'wpd-settings', 'CannaBiz', 'CannaBiz', 'manage_options', 'customize.php', NULL );
-}
-
-/**
  * Metabox to hide title on page-by-page basis
  */
-
 function cannabiz_page_title( $value ) {
 	global $post;
 
