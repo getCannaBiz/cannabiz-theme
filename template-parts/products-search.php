85ac49a4e0b085cb9f79e46157830ed0e9506e2b
Robert DeVore <deviodigital@gmail.com>
2021-08-18 20:48:28 -0400

    :package: NEW: Added products search template file

---
+++ template-parts/products-search.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Template part for displaying results in search pages.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package CannaBiz
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<?php if ( has_post_thumbnail() ) { ?>
+		<a href="<?php the_permalink(); ?>" class="featured-image"><?php the_post_thumbnail( 'wpd-small' ); ?></a>
+	<?php } ?>
+	<header class="entry-header">
+		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+		<div class="entry-meta">
+			<?php cannabiz_posted_on(); ?>
+		</div><!-- .entry-meta -->
+	</header><!-- .entry-header -->
+</article><!-- #post-## -->
